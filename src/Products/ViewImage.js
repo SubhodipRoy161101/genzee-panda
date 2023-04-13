20831bf3f5957a52ee68af964281557b5968f920
SubhodipRoy161101 <subhodiproy161101@gmail.com>
2023-04-13 20:38:15 +0530

    added back btn

--- src/Products/ViewImage.js
+++ src/Products/ViewImage.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./ViewImage.css";
 
 const ViewImage = () => {
+  const navigate = useNavigate();
   return (
     <div className="container">
-      <div className="btn">This is a btn</div>
+      <div className="btn" onClick={() => navigate("/product-details")}>
+        Back
+      </div>
       <div className="row justify-content-around my-4">
-        <div className="col-md-4" style={{ padding: "0px", margin: "0px" }}>
+        <div className="col-md-6" style={{ padding: "0px", margin: "0px" }}>
           <div className="prod-image" style={{ overflow: "scroll" }}>
             <img
               src={localStorage.getItem("imgUrl")}
