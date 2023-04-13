30b9d592dce55119c23da4f3ead5bcd53e21c911
SubhodipRoy161101 <subhodiproy161101@gmail.com>
2023-04-13 20:17:11 +0530

    Edited image view page

--- src/Products/ViewImage.js
+++ src/Products/ViewImage.js
@@ -7,12 +7,12 @@ const ViewImage = () => {
     <div className="container">
       <div className="btn">This is a btn</div>
       <div className="row justify-content-around my-4">
-        <div className="col-md-4">
-          <div className="prod-image">
+        <div className="col-md-4" style={{ padding: "0px", margin: "0px" }}>
+          <div className="prod-image" style={{ overflow: "scroll" }}>
             <img
               src={localStorage.getItem("imgUrl")}
               alt="product View"
-              //   style={{ width: "25vw" }}
+              style={{ width: "100vw" }}
             />
           </div>
         </div>
